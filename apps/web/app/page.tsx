7a817fb5fd1407702f457327b1241e1819bd8afa
ashley taylor <ashleytaylor14@gmail.com>
2024-03-05 20:17:19 +0000

    chore: add divide fn. add changelog file

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,13 @@
 import styles from './page.module.css'
-import { add, subtract } from '@turboapp/math/index'
+import { add, subtract, divide } from '@turboapp/math/index'
 
 export default function Page(): JSX.Element {
   return (
     <main className={styles.main}>
-      <h2>Web {add(4, 2)}</h2>
-      <h2>Web {subtract(4, 2)}</h2>
+      <h1>Web</h1>
+      <h2>Add {add(4, 2)}</h2>
+      <h2>Subtract {subtract(4, 2)}</h2>
+      <h2>Divide {divide(4, 2)}</h2>
     </main>
   )
 }
