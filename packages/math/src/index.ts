e68a36decb381082f80081093de78de460221acb
ashley taylor <ashleytaylor14@gmail.com>
2024-03-06 08:54:37 +0000

    fix(math): add gt fn

--- packages/math/src/index.ts
+++ packages/math/src/index.ts
@@ -7,3 +7,5 @@ export const divide = (num1: number, num2: number) => num1 / num2
 export const multiply = (num1: number, num2: number) => num1 * num2
 
 export const mod = (num1: number, num2: number) => num1 % num2
+
+export const gt = (num1: number, num2: number) => num1 > num2
